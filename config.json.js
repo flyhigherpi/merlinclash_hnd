1a3db6dc7c19478d7fac623ad4b4d7ae43fcb919
flyhigherpi <67776691+flyhigherpi@users.noreply.github.com>
2020-09-04 15:15:18 +0800

    GG

--- config.json.js
+++ config.json.js
@@ -4,7 +4,7 @@
 "home_url":"Module_merlinclash.asp",
 "name":"merlinclash",
 "title":"Merlin Clash",
-"version":"0902",
+"version":"0904",
 "patch_version":"000",
 "sc_version":"2020082201",
 "com_version":"2020081701",
