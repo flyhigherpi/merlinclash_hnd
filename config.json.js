ad64243570dfe4d7636d9333760d629b70481380
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-11-08 00:17:53 +0800

    y

--- config.json.js
+++ config.json.js
@@ -4,8 +4,8 @@
 "home_url":"Module_merlinclash.asp",
 "name":"merlinclash",
 "title":"Merlin Clash",
-"version":"1103",
-"patch_version":"003",
+"version":"1106",
+"patch_version":"000",
 "sc_version":"2020090801",
 "com_version":"2020081701",
 "game_version":"2020071901"
