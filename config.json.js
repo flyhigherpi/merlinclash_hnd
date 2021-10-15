85ce2cf58129c50a2a4664656d1afad13e52cac8
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-10-16 00:32:11 +0800

    Y

--- config.json.js
+++ config.json.js
@@ -6,7 +6,7 @@
 "title":"Merlin Clash",
 "version":"20211007",
 "patch_version":"004",
-"sc_version":"2021092201",
+"sc_version":"2021101601",
 "com_version":"2021060601",
 "game_version":"2020071901"
 }
